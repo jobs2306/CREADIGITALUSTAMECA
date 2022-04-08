95dca5a409871ca44841ebdeb1e776134207c30c
JoelB <joeebaena@gmail.com>
2022-04-08 08:25:58 -0500

    commitsalto
    Ahora se puede saltar

--- Assets/Scripts/movimientojugador.cs
+++ Assets/Scripts/movimientojugador.cs
@@ -5,29 +5,49 @@ using UnityEngine;
 public class movimientojugador : MonoBehaviour
 {
         public float velocidad = 10;
-        public float velocidadSalto;
-    
+        Animator jugadoranim;
+        SpriteRenderer jugadorRender;
+        bool VoltearJugador;
+        Rigidbody2D jugadorRB;
+        float fuerzaSalto = 5f;
+        int Limsaltos = 2;
+        int saltosHechos = 0;
+
     void Start()
     {
-        
+        jugadorRB = GetComponent<Rigidbody2D>();
+        jugadoranim = GetComponent<Animator>();
+        jugadorRender = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0,0,1.19f);
-        if (Input.GetKeyDown("left"))
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }
-        if (Input.GetKeyDown("right"))
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
-        
-            gameObject.GetComponent<Transform>().Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * velocidad, 0, 0));
-                   
+        float movimiento = Input.GetAxis("Horizontal");
 
+        if (movimiento > 0 && !VoltearJugador)
+        {
+            Voltear();
+        }
+        else if (movimiento <0 && VoltearJugador)
+        {
+            Voltear();
+        }
+        jugadorRB.velocity = new Vector2(movimiento*velocidad, jugadorRB.velocity.y);
+        jugadoranim.SetFloat("VelMovimiento",Mathf.Abs(movimiento));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (saltosHechos<Limsaltos)
+            {
+                jugadorRB.AddForce(new Vector2(0f,fuerzaSalto),ForceMode2D.Impulse);
+                saltosHechos++;
+            }
+        }
     }
 
-
+    void Voltear()
+    {
+        VoltearJugador = !VoltearJugador;
+        jugadorRender.flipX = !jugadorRender.flipX;
+    }
 }
