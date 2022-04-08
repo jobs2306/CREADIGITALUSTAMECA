31217ec7a6a005ea6dd9722c0b2ca23e97b57a59
Mauricio Gómez <magma_19699@hotmail.com>
2022-04-07 23:41:27 -0500

    Cambios barra de vida
    El icono de la barra de vida se acopla al tamaño que tenga la barra de vida

--- Assets/Scripts/Contadores_Obstaculos_Andres.cs
+++ Assets/Scripts/Contadores_Obstaculos_Andres.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Contadores_Obstaculos_Andres : MonoBehaviour
 {
-    
+    public GameObject barraVida;
     int contHA; //Contador hongos amarillos
     int contHR; //Contador hongos rojos
     int contTO; //Contador trampas osos
     int contTP; //Contador trampa petroleo
-    //GameObject maxHealth = GameObject.Find("LifeBar").GetProperty(localScale);
-    float maxHealth = 20f; //tamaño de la barra de vida
+    float maxHealth;
     float minRange;
     float maxRange;
     void Start()
     {
-        maxRange = transform.position.x;
+        maxHealth = barraVida.transform.localScale.x;
+        transform.position = new Vector3(barraVida.transform.position.x + (maxHealth)/2, barraVida.transform.position.y, 0);
+        maxRange = barraVida.transform.position.x + (barraVida.transform.localScale.x)/2;
         minRange = maxRange -maxHealth;
     }
     
